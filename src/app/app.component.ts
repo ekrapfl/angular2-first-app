d2ed416169de993ac910c1568459d2d57e92869c
Eben Krapfl <eben@krapfl.org>
2017-02-14 16:54:51 -0600

    Up to Section 2 Lecture 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>
+      {{title}}
+    </h1>
+    <fa-other></fa-other>
+    <fa-another></fa-another>
+  `,
+  styles: [`
+    h1 {
+      color: red;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'ITS ALIVE!';
 }
